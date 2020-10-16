08cd35fa296d58ae33bcc7c236499162b80848c3
Christoph Schwer <christoph.schwer@analytical-software.de>
2020-10-16 08:46:55 +0200

    Change random tips to display < and > properly

--- random-tip.js
+++ random-tip.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   var phrase = phraseArray[Math.floor(Math.random() * phraseArray.length)];
-  document.getElementById("tip").innerHTML = phrase; 
+  document.getElementById("tip").appendChild(document.createTextNode(phrase));
 });
 
